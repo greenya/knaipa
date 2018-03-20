f2b3d0ee6960855fc5142f04296d6d45acc56249
grinevich <yuriy.grinevich@novage.com.ua>
2018-03-20 19:21:57 +0200

    settings: libs list

--- app-i18n.js
+++ app-i18n.js
@@ -9,10 +9,15 @@ function appI18n() {
                 'settings':             'Settings',
                 'total-members-shown': '{shown} of {total} members shown',
                 'language':             'Language',
+                'libraries':            'Libraries',
                 'loading':              'Loading...',
+                'no-members-found':     'No members found.',
+                'page-not-found':       'Page Not Found',
+                'page-not-found-text':  'The page you are looking for could not be found.',
                 'theme':                'Theme',
                 'theme-light':          'Light',
                 'theme-dark':           'Dark',
+                'version':              'Version',
             },
             ua: {
                 'guild-title':          'Кнайпа Варʼятів',
@@ -22,10 +27,15 @@ function appI18n() {
                 'settings':             'Налаштування',
                 'total-members-shown': '{shown} з {total} варʼятів відображено',
                 'language':             'Мова',
+                'libraries':            'Бібліотеки',
                 'loading':              'Завантаження...',
+                'no-members-found':     'Варʼятів не знайдено.',
+                'page-not-found':       'Сторінку не знайдено',
+                'page-not-found-text':  'Сторінки, яку ви запитуєте, більше не існує.',
                 'theme':                'Оформлення',
                 'theme-light':          'Світле',
                 'theme-dark':           'Темне',
+                'version':              'Версія',
             }
         }
     });
